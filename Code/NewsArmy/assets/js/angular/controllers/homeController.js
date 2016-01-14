509f3f46850d71388288e4f90129ad9a1113405a
faridsaud <faridsaud@yahoo.com>
2016-01-14 17:58:24 -0500

    Project 100% menos sockets

--- Code/NewsArmy/assets/js/angular/controllers/homeController.js
+++ Code/NewsArmy/assets/js/angular/controllers/homeController.js
@@ -1,22 +1,23 @@
 applicacion.controller('homeController',['$scope','categoriaFactory','noticiaFactory',function($scope,categoriaFactory,noticiaFactory){
+
+    io.socket.on('noticia', function (msg) {
+
+      // Let's see what the server has to say...
+      switch(msg.verb) {
+
+        case 'created':
+
+        console.log("socket create");
+        $scope.ultimas6Noticias.pop();
+        $scope.ultimas6Noticias.push(msg.data); // (add the new order to the DOM)
+        $scope.$apply();      // (re-render)
+        break;
+
+        default: return; // ignore any unrecognized messages
+      }
+    });
+
   console.log("homeController");
-
-  io.socket.on("noticia", function (msg) {
-
-    // Let's see what the server has to say...
-    switch(msg.verb) {
-
-      case 'created':
-
-      $scope.ultimas6Noticias.pop();
-      console.log("socket create");
-      $scope.ultimas6Noticias.push(msg.data); // (add the new order to the DOM)
-      $scope.$apply();      // (re-render)
-      break;break;
-
-      default: return; // ignore any unrecognized messages
-    }
-  });
 
   //$scope.categorias=categoriaFactory.query();
   categoriaFactory.query().$promise.then(function success(results){
@@ -31,12 +32,8 @@ applicacion.controller('homeController',['$scope','categoriaFactory','noticiaFac
   ).$promise.then(function success(results){
     $scope.ultimas6Noticias=results;
     console.log(results);
-
-    console.log($scope.ultimas6Noticias[1].imagenes);
   }, function error(error){
     console.log(error);
   })
-  $scope.usuarioLogeado={id:1};
-
 
 }]);
